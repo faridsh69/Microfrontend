3d267cedc2096740b89a996a4a497dd2334a3486
Farid Shahidi <shahidi@innoscripta.com>
2024-01-23 10:16:40 +0100

    fixed .env

--- best-practices-micro/src/services/apis.ts
+++ best-practices-micro/src/services/apis.ts
@@ -9,35 +9,35 @@ if (!REACT_APP_API_BASE_URL) {
   throw new Error('Please copy .env.example to .env.local')
 }
 
-const VITE_AUTH_API_CLIENT = createApiClient(REACT_APP_API_BASE_URL)
+const AUTH_API_CLIENT = createApiClient(REACT_APP_API_BASE_URL)
 
 export const postLogin: TypeApiMethod = data =>
-  VITE_AUTH_API_CLIENT.post({
+  AUTH_API_CLIENT.post({
     endpoint: 'oauth/token',
     data,
   })
 
-const TEST_API_CLIENT = createApiClient(`https://reqres.in/api/users`, true)
+const USER_API_CLIENT = createApiClient(REACT_APP_API_BASE_URL, true)
 
 export const getUsers: TypeApiMethod = data =>
-  TEST_API_CLIENT.get({
+  USER_API_CLIENT.get({
     endpoint: '',
     data,
   })
 
 export const createUser: TypeApiMethod = data =>
-  TEST_API_CLIENT.post({
+  USER_API_CLIENT.post({
     endpoint: '',
     data,
   })
 
 export const updateUser: TypeApiMethod = data =>
-  TEST_API_CLIENT.put({
+  USER_API_CLIENT.put({
     endpoint: `id/${data.id}`,
     data,
   })
 
 export const deleteUser: TypeApiMethod = data =>
-  TEST_API_CLIENT.remove({
+  USER_API_CLIENT.remove({
     endpoint: `id/${data}`,
   })
